91e0f8d5e6f4265256b375c212170674f86d83f1
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-06-29 12:31:29 +0300

    Completed Exercise 43

---
+++ Exercise_43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Input your b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Your data is: b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}");
+
+// y = k1 * x + b1, y = k2 * x + b2
+// k1* x + b1 = k2 * x + b2
+// k1 * x - k2 * x = b2 - b1
+// x * (k1 - k2) = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+
+
+void FindIntersectionPoint(double pointB1, double pointK1, double pointB2, double pointK2)
+{
+    double x = (pointB2 - pointB1) / (pointK1 - pointK2);
+    double y = (pointK1 * x + pointB1);
+    Console.WriteLine($"Your result is ({x}, {y})");
+}
+
+FindIntersectionPoint(b1, k1, b2, k2);
